9a6b9f45907bdc6fca8576abab4719c8ebb031b2
Matthew <matthew_mcaleer@hotmail.com>
2022-07-15 12:37:05 +1000

    validations added

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,9 +1,10 @@
 class Trip < ApplicationRecord
   #belongs_to :user
   belongs_to :place
+  validates :name, presence: true
 
   def place_name=(name)
-    self.place = Place.find_or_create_by(name: name)
+    self.place = Place.find_or_create_by(name: name.titleize)
   end
 
   def place_name
